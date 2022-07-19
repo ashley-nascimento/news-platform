1ddd4cbec3cdd6132dac4fdcbe3f4288cc974657
Ashley <ashley_nascimento@hotmail.com>
2022-07-19 00:00:16 -0300

    adding adjust

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -43,7 +43,7 @@ export default NextAuth({
             q.Get(
               q.Match(
                 q.Index('user_by_email'),
-                q.Casefold(user.email)
+                q.Casefold(user.email),
               )
             )
           )
