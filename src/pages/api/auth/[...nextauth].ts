8100e48bd916b380377f2685156be78616bbacff
Ashley <ashley_nascimento@hotmail.com>
2022-07-18 23:47:57 -0300

    adding fauna

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,14 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+import { fauna } from '../../../services/fauna'
+
+import { query as q } from 'faunadb'
+
 export default NextAuth({
   providers: [
     GithubProvider({
+      checks: "state",
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
       authorization: {
@@ -13,4 +18,42 @@ export default NextAuth({
       }
     }),
   ],
+  jwt:{
+    secret: process.env.NEXTAUTH_SECRET
+  },
+  callbacks:{
+    async signIn(user){
+      const { email } = user.user
+
+      try{
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true
+      }
+      catch{
+        console.log('deu erro')
+        return false
+      }
+    }
+  }
 })
