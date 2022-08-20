ed4410571df0d2bf965c6d5061b1763b1d658ccb
Ashley <ashley_nascimento@hotmail.com>
2022-08-19 21:42:40 -0300

    adding lib user ref

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -1,7 +1,29 @@
+import { fauna } from "../../../services/fauna"
+import { query as q } from 'faunadb'
+import { stripe } from "../../../services/stripe"
+
 export async function saveSubscription(
     subscriptionId: string, 
     costumerId: string
     ){
         
    console.log(subscriptionId, costumerId)
+
+   const userRef =  await fauna.query(
+
+        q.Select(
+            'ref',
+            q.Get(
+                q.Match(
+                    q.Index('user_by_stripe_costumer_id'),
+                    costumerId
+                )
+            )
+        )
+    )
+    const subscription =  await stripe.subscriptions.retrieve(subscriptionId)
+
+    await fauna.query(
+        
+    )
 }
